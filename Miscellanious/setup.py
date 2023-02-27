f3e32cab2e9a97516b986ab35b14c984e61b9f25
Jewdah18 <jdrelich7@gmail.com>
2023-02-27 03:09:06 -0500

    Initial commit

---
+++ Miscellanious/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analyzing TCP data dumps to predict network intrusions',
+    author='Judah Drelich',
+    license='',
+)
