65bcbba4b05c53cc583c29ebcb4a36f8c287b7e7
Jewdah18 <jdrelich7@gmail.com>
2023-03-08 00:19:25 -0500

    Saving

--- notebooks/test.r
+++ notebooks/test.r
@@ -12,19 +12,36 @@ setwd("C:/Users/jdrel/OneDrive/Documents/Data_Science/Springboard/Capstone-2")
 data <- read.csv("./data/interim/X_scaled.csv")
 clusters <- read.csv("./data/interim/clusters.csv")
 
-# Separate out numerical and categorical
-data[, (ncol(data) - 6):ncol(data)] <- lapply(data[, (ncol(data) - 6):ncol(data)], factor)
+# Create a loop for numbers 36-42
+for (i in 36:42) {
+    # Reassign those columns as factors
+    data[i] <- lapply(data[i], factor)
+}
 
-# Create dissimilarity matrix to compare all the different points
-dissimilarity_matrix <- daisy(data, metric = "gower")
+#Initialize Silhouette Scores
+sil_score <- numeric()
+# Initialize dissimilarity matrix for each row
+diss_mat <- numeric()
 
-print(head(dissimilarity_matrix))
+# Create the first loop that will loop through all the rows
+for (i in seq_len(1)) {
+    for (j in seq_len(nrow(data))) {
+        # combine two rows at a time to make it small enough to handle
+        rows <- rbind(data[1, ], data[j, ])
+        # Create dissimilarity matrix to compare the points in the selected rows
+        diss_mat_temp <- daisy(rows, metric = "gower",
+                        type = list(as.integer = seq_len(35),
+                                    as.character = 36:42))
+        # Change dissmat to a list so it can be flattened
+        diss_mat <- c(diss_mat, list(diss_mat_temp))
+  }
+    cat("The length of the dissimilarity matrix is:", diss_mat)
+    # Get the label for that specific row
+    label <- clusters$cluster[i]
+    cat("and the cluster label is:", label)
+    # Run the silhouette score for that row
+    sil_score <- c(sil_score, silhouette(diss_mat, label)[, 3])
+}
 
-#import the clusters from EDA
-clus <- clusters$cluster
-
-# Compute the silhouette score with a dissimilarity matrix that uses Gower
-# distance so that both numerical and categorical variables are taken into
-# account
-silhouette_coefficient <- silhouette(clus, dissimilarity_matrix)
-mean_silhouette_coefficient <- mean(silhouette_coefficient[, 3])
+# Print out the silhouette score
+print(mean(sil_score))
